0bfa1ea715db7085ed51acbca84856612dcf9970
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-07-15 17:10:06 +0200

    no invio

--- database/seeds/TripsTableSeeder.php
+++ database/seeds/TripsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Trips;
+use App\Trip; //collegato al model
 
 class TripsTableSeeder extends Seeder
 {
@@ -12,7 +12,33 @@ class TripsTableSeeder extends Seeder
      */
     public function run()
     {
-        $arrTrips = config('trip');
-        dd($arrTrips);
+        $arrTrips = config('trips');
+        dd($arrTrips); // adesso come faccio a fare debug ?
+
+        
+        foreach ($arrTrips as $item) {
+            
+            $newTrip = new Trip();
+            
+            $newTrip->nome_struttura = $item["nome_struttura"];
+            $newTrip->citta = $item["citta"];
+            $newTrip->stato = $item["stato"];
+            $newTrip->cap = $item["cap"];
+            $newTrip->via = $item["via"];
+            $newTrip->voto = $item["voto"];
+            $newTrip->descrizione = $item["descrizione"];
+            $newTrip->recensione = $item["recensione"];
+            $newTrip->partenza = $item["partenza"];
+            $newTrip->destinazione = $item["destinazione"];
+            $newTrip->cancellazione_gratis = $item["cancellazione_gratis"];
+            $newTrip->posti_disponibili = $item["posti_disponibili"];
+            $newTrip->sconto = $item["sconto"];
+            $newTrip->andata = $item["andata"];
+            $newTrip->ritorno = $item["ritorno"];
+
+            $newTrip->save();
+
+        }
+
     }
 }
